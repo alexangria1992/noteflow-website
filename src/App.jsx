9900667d8a5e9b995bb5161a61a07f11d22b6d8c
Alexandre Angrignon <alex040892@gmail.com>
2025-04-30 22:40:08 +1200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './sections/Header';
+import Page from './sections/Page';
+
+function App() {
+  return (
+    <Page>
+      <Header />
+    </Page>
+  );
+}
+
+export default App;
