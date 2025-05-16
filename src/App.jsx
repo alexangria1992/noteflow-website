78842a788ffa6e0d2ac529e75a456f92d7859be2
Alexandre Angrignon <alex040892@gmail.com>
2025-05-16 15:22:53 +1200

    Desktop Features

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+// Page
+
+import Page from './components/sections/Page';
+
+//Header
+
 import Header from './components/sections/Header';
 import Hero from './components/sections/Hero';
+import Reviews from './components/sections/Reviews';
+import Navigation from './components/sections/Navigation';
+
+// Main
 import Logos from './components/sections/Logos';
 import Main from './components/sections/Main';
-import Navigation from './components/sections/Navigation';
-import Page from './components/sections/Page';
-import Reviews from './components/sections/Reviews';
+
+import Features from './components/sections/Features';
 
 function App() {
   return (
@@ -16,6 +25,7 @@ function App() {
       </Header>
       <Main>
         <Logos />
+        <Features />
       </Main>
     </Page>
   );
